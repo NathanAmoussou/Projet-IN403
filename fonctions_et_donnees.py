73c3c12eeb4042bdb27e76af39085a851ec3f5a1
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2023-04-27 13:25:38 +0200

    Création de Sommet() et Arcs() et exemples

--- fonctions_et_donnees.py
+++ fonctions_et_donnees.py
@@ -2,18 +2,24 @@
 # Nom De Piste : Station de nom Nom De Piste
 # Nom De Piste H : Haut de la piste de nom Nom De Piste
 # Nom De Piste B : Bas de la piste de nom Nom De Piste
-# NOM DE PISTE H : Haut de la remontée mécanique de nom NOM DE PISTE
-# NOM DE PISTE B : Bas de la remontée mécanique de nom NOM DE PISTE
+# 1NOM DE PISTE H : Haut du téléski de nom NOM DE PISTE
+# 1NOM DE PISTE B : Bas du téléski de nom NOM DE PISTE
+# 2NOM DE PISTE H : Haut du télésiège de nom NOM DE PISTE
+# 2NOM DE PISTE B : Bas du télésiège de nom NOM DE PISTE
+# 3NOM DE PISTE H : Haut de la télécabine de nom NOM DE PISTE
+# 3NOM DE PISTE B : Bas de la télécabine de nom NOM DE PISTE
+# 4NOM DE PISTE H : Haut du téléphérique de nom NOM DE PISTE
+# 4NOM DE PISTE B : Bas du téléphérique de nom NOM DE PISTE
 # Le haut pour une piste est le point de départ
-# 
+# Le bas pour une piste est le point d'arrivée
+# Le haut pour une remontée mécanique est le point de départ
+# Le bas pour une remontée mécanique est le point d'arrivée
+
 SOMMETS = [
     {'Nom': ['Chanrossa', 'Roc Merlet H', 'Jean Pachod H', 'Chanrossa H', 'ROC MERLET B', 'CHANROSSA B'],
      'd-': ['ROC MERLET', 'CHANROSSA'],
      'd+': ['Roc Merlet', 'Jean Pachod', 'Chanrossa'],
-     'duree': 0},
-    {'Nom': ['Plan Mugnier H', 'Mont Russes H', 'Pyramide H', 'Roc Merlet B', 'ROC MERLET H', 'PYRAMIDE B'],
-     }
-    
+     'duree': 0}
 ]
 
 SOMMETS = [
